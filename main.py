4da3659dfd21ab65707b2484c8de5e2aff36e294
Nathan Campos <eeepc904@gmail.com>
2014-02-23 09:48:28 -0300

    Started working with Flask

---
+++ main.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python
+
+import json
+import flask
+
+import api.api as API
+
+app = flask.Flask(__name__)
+
+@app.route("/api")
+def api_request():
+    api = API.API()
+    final_hash = { "stocks": None,
+                   "weather": None }
+
+    final_hash["stocks"] = api.get_stock("GOOG")
+    final_hash["weather"] = api.get_weather("Vitoria, Espirito Santo")
+
+    return flask.Response(json.dumps(final_hash),
+                          content_type = "application/json; charset=utf-8")
+
+if __name__ == "__main__":
+    app.debug = True
+    app.run()
